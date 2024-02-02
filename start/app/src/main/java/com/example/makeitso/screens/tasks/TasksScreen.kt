e9fcc337b9d3998f03e269a840c8f1b1708e3c06
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-02-02 19:05:01 +0100

    Funcionalidad final

--- start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
+++ start/app/src/main/java/com/example/makeitso/screens/tasks/TasksScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.makeitso.R.drawable as AppIcon
 import com.example.makeitso.R.string as AppText
 import com.example.makeitso.common.composable.ActionToolbar
@@ -42,7 +43,9 @@ fun TasksScreen(
   openScreen: (String) -> Unit,
   viewModel: TasksViewModel = hiltViewModel()
 ) {
+  val tasks = viewModel.tasks.collectAsStateWithLifecycle(emptyList())
   TasksScreenContent(
+    tasks = tasks.value,
     onAddClick = viewModel::onAddClick,
     onSettingsClick = viewModel::onSettingsClick,
     onTaskCheckChange = viewModel::onTaskCheckChange,
@@ -58,6 +61,7 @@ fun TasksScreen(
 @ExperimentalMaterialApi
 fun TasksScreenContent(
   modifier: Modifier = Modifier,
+  tasks: List<Task>,
   onAddClick: ((String) -> Unit) -> Unit,
   onSettingsClick: ((String) -> Unit) -> Unit,
   onTaskCheckChange: (Task) -> Unit,
@@ -87,7 +91,8 @@ fun TasksScreenContent(
       Spacer(modifier = Modifier.smallSpacer())
 
       LazyColumn {
-        items(emptyList<Task>(), key = { it.id }) { taskItem ->
+
+        items(tasks, key = {it.id}) { taskItem ->
           TaskItem(
             task = taskItem,
             options = listOf(),
@@ -104,8 +109,14 @@ fun TasksScreenContent(
 @ExperimentalMaterialApi
 @Composable
 fun TasksScreenPreview() {
+  val task = Task(
+    title = "Task title",
+    flag = true,
+    completed = true
+  )
   MakeItSoTheme {
     TasksScreenContent(
+      tasks = listOf(task),
       onAddClick = { },
       onSettingsClick = { },
       onTaskCheckChange = { },
